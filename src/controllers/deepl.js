f44cc4367675e7a3ff55a74468ded08bdeacfa00
juanan-fernandez <juanan.fernandez@gmail.com>
2026-01-12 14:03:07 +0100

    versión de la pasarela usando el cliente para node de deepl

--- src/controllers/deepl.js
+++ src/controllers/deepl.js
@@ -1,63 +1,41 @@
-// function encodeURLString(input) {
-// 	return input.replace(/[!*'();:@&=+$,/?%#\[\]]/g, function (match) {
-// 		return encodeURIComponent(match)
-// 	})
-// }
-
-// Ejemplo de uso:
-
-//deprecada desde 15-01-2026 => refactorizar y eliminar código inutil en fechas posteriores
-const getTranslation = async (req, res) => {
-	const text = req.params.text
-	const target = req.params.target
-
-	//const originalString = "Hola! ¿cómo estás? Me llamo Escarlata O'Hara";
-	//const encodedString = encodeURLString(originalString);
-
-	try {
-		const response = await fetch(
-			`https://api.deepl.com/v2/translate?auth_key=${process.env.DEEPL_KEY}&text=${text}&target_lang=${target}`
-		)
-		const data = await response.json()
-		res.status(200).json({
-			text: data.translations[0].text
-		})
-	} catch (err) {
-		console.log(err)
-		res.status(500).json({
-			text: 'ERROR: En la llamada a la API',
-			error: err
-		})
-	}
-}
+const deepl = require('deepl-node')
 
 //petición válida desde 15-01-2026
 const getTranslationV2 = async (req, res) => {
-	const url = 'https://api.deepl.com/v2/translate'
+	//const url = 'https://api.deepl.com/v2/translate'
 	const authKey = process.env.DEEPL_KEY
+	const deeplClient = new deepl.Translator(authKey)
+
 	try {
-		const requestBody = { text: [req.params.text], target_lang: req.params.target }
-		const response = await fetch(url, {
-			method: 'POST',
-			headers: {
-				// La especificación pide la clave directamente en Authorization
-				Authorization: `DeepL-Auth-Key ${authKey}`,
-				'Content-Type': 'application/json',
-				'User-Agent': 'Samar/1.2.3'
-			},
-			body: JSON.stringify(requestBody)
-		})
-
-		if (!response.ok) {
-			const errDetail = await response.text()
-			throw new Error(`No funcionó la peticición a la api - ${response.status} - ${errDetail}`)
-		}
-
-		const data = await response.json()
-		console.log(data)
+		let target = req.params.target
+		if (target.toUpperCase().startsWith('EN')) target = 'en-gb'
+		const result = await deeplClient.translateText(req.params.text, 'es', target)
+		console.log(result)
 		res.status(200).json({
-			text: data.translations[0].text
+			text: result.text
 		})
+		// const requestBody = { text: [req.params.text], target_lang: req.params.target }
+		// const response = await fetch(url, {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		// La especificación pide la clave directamente en Authorization
+		// 		Authorization: `DeepL-Auth-Key ${authKey}`,
+		// 		'Content-Type': 'application/json',
+		// 		'User-Agent': 'Samar/1.2.3'
+		// 	},
+		// 	body: JSON.stringify(requestBody)
+		// })
+
+		// if (!response.ok) {
+		// 	const errDetail = await response.text()
+		// 	throw new Error(`No funcionó la peticición a la api - ${response.status} - ${errDetail}`)
+		// }
+
+		// const data = await response.json()
+		// console.log(data)
+		// res.status(200).json({
+		// 	text: data.translations[0].text
+		// })
 	} catch (err) {
 		console.error(err)
 		res.status(500).json({
@@ -67,4 +45,4 @@ const getTranslationV2 = async (req, res) => {
 	}
 }
 
-module.exports = { getTranslation, getTranslationV2 }
+module.exports = { getTranslationV2 }
--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ process.loadEnvFile('.env')
 
 app.use('/api/v1', v1Router)
 
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+	console.log(`Deepl Client running on port ${PORT}`)
 })
